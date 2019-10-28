6c8621aca3cf07522db7b997b5527fec4b4b01ed
viveksacademia4git <viveksacademia@gmail.com>
2019-10-28 14:12:55 +0100

    Java Project Creation

---
+++ JavaScanWEP/src/main/java/edu/srh/is/ScanWEP.java
@@ -0,0 +1,21 @@
+package edu.srh.is;
+
+import org.json.JSONObject;
+
+/**
+ * Util class to perform the scanning of the WEP networks
+ * @author Vivek Yadav
+ */
+public class ScanWEP {
+
+
+	/**
+	 * Function to scan the WEP Network and return the details.
+	 * @param wepNetworkName {@link String}
+	 * @return jsonWepDetail {@link JSONObject}
+	 */
+	public static JSONObject scanWep(String wepNetworkName) {
+		return null;
+	}
+
+}
