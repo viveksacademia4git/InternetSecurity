b38ef7a56fa363da7ec94734ac95859a134404a0
viveksacademia4git <viveksacademia@gmail.com>
2019-10-30 22:20:00 +0100

    Initial Java Project Skeleton

---
+++ JavaManInTheMiddleAttack/src/test/java/edu/srh/is/ApplicationTest.java
@@ -0,0 +1,24 @@
+package edu.srh.is;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ApplicationTest {
+
+	@Test
+	void testMain_Null() {
+		Throwable exception = assertThrows(NullPointerException.class, () -> {
+			Application.main(null);
+        });
+        assertEquals(exception.getMessage(), Application.NULL_MESSAGE);
+	}
+
+	@Test
+	void testMain_Value() {
+		String[] args = { "Hello" };
+		Application.main(args);
+		assertEquals( Application.MESSAGE , args[0]);
+	}
+
+}
